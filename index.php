22e44a7c5b25ea8515a2554216c52698158f1b81
minhkhang97 <maxwelljenkins2005@gmail.com>
2020-11-25 09:00:33 +0700

    dadhasghj

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="logo">
+                    <img src="./images/logo-white.png" alt="">
+                </div>
+
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">food delivery</a></li>
+                        <li><a href="#">how it works</a></li>
+                        <li><a href="#">out cities</a></li>
+                        <li><a href="#">sign up</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="header-context">
+                <h1>goodbye junk food.</h1>
+                <h1> hello super healthy meals</h1>
+                <div class="btn-group">
+                    <a href="#">i'm hungry</a>
+                    <a href="3">show me more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
